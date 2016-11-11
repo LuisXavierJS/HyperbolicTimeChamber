815e8a7f57e2fa3d3f31595a7dcb9a2e3c122c23
LuisXavierJS <oojlxavier@gmail.com>
2016-11-11 07:25:46 -0200

    Componente de cabeçalho

---
+++ cabecalho.component/cabecalho.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'cabecalho-fintech',
+    templateUrl: './app/cabecalho.component/cabecalho.component.html',
+    styleUrls: ['./app/cabecalho.component/cabecalho.component.css'],
+})
+export class CabecalhoComponent {
+
+}
